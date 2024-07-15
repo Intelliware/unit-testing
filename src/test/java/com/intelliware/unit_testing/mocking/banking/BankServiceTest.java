a4b782d10a6f0f80b89fe33d148b5cbae9185b2c
Graham Foster <graham.foster@intelliware.ca>
2024-07-14 21:03:02 -0400

    Added BankServiceTest with failing tests

---
+++ src/test/java/com/intelliware/unit_testing/mocking/banking/BankServiceTest.java
@@ -0,0 +1,44 @@
+package com.intelliware.unit_testing.mocking.banking;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.intelliware.unit_testing.mocking.banking.adapters.ScotiaBankAdapter;
+import com.intelliware.unit_testing.mocking.banking.adapters.TDAdapter;
+
+public class BankServiceTest {
+
+  private BankService fixture;
+
+  @Mock
+  private ScotiaBankAdapter scotiaBankAdapter;
+
+  @Mock
+  private TDAdapter tdAdapter;
+
+  @BeforeEach
+  public void setup() {
+    MockitoAnnotations.openMocks(this);
+
+    fixture = new BankService(scotiaBankAdapter, tdAdapter);
+  }
+
+  @Test
+  public void testBankServiceTransfersFromScotia() {
+    Boolean actual = fixture.getFunds(null, null);
+
+    assertTrue(actual);
+  }
+
+  @Test
+  public void testBankServiceTransferFailsToTD() {
+    Boolean actual = fixture.getFunds(null, null);
+
+    assertFalse(actual);
+  }
+}
